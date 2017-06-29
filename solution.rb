72612b27dcd3da3e4bb7036262872181d70375bf
josepaez2 <josepaez_2@hotmail.com>
2017-06-29 00:16:20 -0500

    version2

--- solution.rb
+++ solution.rb
@@ -1,11 +1,12 @@
 require "sinatra"
 
 get "/" do
-	nombre = params["nombre"]
-	if !nombre
-		"Hola desconocido!"
-	else
-	 "Hola #{nombre}!"
-	end	
+	# nombre = params["nombre"]
+	# if !nombre
+	# 	"Hola desconocido!"
+	# else
+	#  "Hola #{nombre}!"
+	# end	
+	"Hola Juan!"
 end
 
