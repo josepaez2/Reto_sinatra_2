7b6b06210325b16ccf90ced9f9523d54dca1d60a
josepaez2 <josepaez_2@hotmail.com>
2017-06-29 01:23:01 -0500

    versionFINAL

--- solution.rb
+++ solution.rb
@@ -1,22 +1,12 @@
 require "sinatra"
 
-# get "/makers" do
-
-#   path = request.fullpath
-#   path = path[8..16]
-#   path = path.capitalize
-# 	"Hola #{path}!"
-# 	# arequest.url 
-# end
 
 get "/makers/:name" do
-  # YOUR CODE HERE
   	a = params[:name]
   	a.capitalize!
    "Hola #{a}!"
 end
 
 
-
-# # LINK DEMASIADO ÜTIL
+# # LINK PODRÍA SER ÜTIL
 # http://www.sinatrarb.com/faq.html#path_info
